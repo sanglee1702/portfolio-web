d56b103d31ec4ad4265eeb0058b0209fd7f740a4
sanglee <lyphuocsang96@gmail.com>
2025-04-11 01:54:33 +0700

    fix css

--- app/routes/home/displacement-sphere.jsx
+++ app/routes/home/displacement-sphere.jsx
@@ -19,8 +19,8 @@ import {
 import { media } from '~/utils/style';
 import { throttle } from '~/utils/throttle';
 import { cleanRenderer, cleanScene, removeLights } from '~/utils/three';
-import fragmentShader from './displacement-sphere-fragment.ts';
-import vertexShader from './displacement-sphere-vertex.ts';
+import fragmentShader from './displacement-sphere-fragment.glsl?raw';
+import vertexShader from './displacement-sphere-vertex.glsl?raw';
 import styles from './displacement-sphere.module.css';
 
 const springConfig = {
