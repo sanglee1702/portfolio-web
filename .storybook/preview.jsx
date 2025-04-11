6b5dd2af0cb763a22cac6950e700bdf1995d0ba3
sanglee <lyphuocsang96@gmail.com>
2025-04-11 22:16:17 +0700

    rollback

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { ThemeProvider, themeStyles } from '../app/components/theme-provider';
 import '../app/reset.module.css';
-import '../app/global.css';
+import '../app/global.module.css';
 import './preview.css';
 
 export const decorators = [
