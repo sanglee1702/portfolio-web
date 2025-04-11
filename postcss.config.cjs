d1ac7fbfb4167fcf60cf0e1fec381b9ccb295040
sanglee <lyphuocsang96@gmail.com>
2025-04-11 21:25:55 +0700

    fix font

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
     '@csstools/postcss-global-data': {
       files: ['app/global.module.css'],
     },
+    'postcss-import': {},
+    'postcss-nesting': {}, // enables nesting like :global inside layers
     'postcss-custom-media': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'custom-media-queries': true,
+        'nesting-rules': true,
+      },
+    },
   },
 };
