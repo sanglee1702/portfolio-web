25b9055c90609be5f78ef01a0e3811fc5333da26
sanglee <lyphuocsang96@gmail.com>
2025-04-11 21:56:27 +0700

    fix css

--- app/root.jsx
+++ app/root.jsx
@@ -19,7 +19,7 @@ import { Progress } from '~/components/progress';
 import config from '~/config.json';
 import styles from './root.module.css';
 import './reset.module.css';
-import './global.module.css';
+import './global.css';
 
 export const links = () => [
   {
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: {
-    '@csstools/postcss-global-data': {
-      files: ['app/global.module.css'],
-    },
     'postcss-custom-media': {},
   },
 };
